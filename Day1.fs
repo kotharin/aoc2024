208d763583ead0022fd8de7c5401641e3b9269d7
Neerav Kothari <neerav.kothari@gmail.com>
2024-12-11 10:54:18 -0500

    Day1 Part2

--- Day1.fs
+++ Day1.fs
@@ -26,3 +26,41 @@ module Part1 =
             |> Array.sum
 
         xx
+
+module Part2 =
+
+    open System
+    open System.IO
+
+    let solution file =
+        let lines = File.ReadAllLines file
+
+        let left, right =
+            lines
+            |> Array.map(fun line ->
+                let vals = line.Split([|' '|])
+                int(vals.[0].Trim()), int(vals.[3].Trim())
+            ) 
+            |> Array.unzip
+
+        let rightGroups =
+            right
+            |> Array.fold(fun map num ->
+                // check if the number exists inthe map, if so increment count
+                let currentCount =
+                    Map.tryFind num map
+                    |> Option.defaultValue 0
+                Map.add num (currentCount + 1) map
+            ) Map.empty
+
+        let similarityScore =
+            left
+            |> Array.map(fun leftNum ->
+                // check if the number exists in the rightGroups
+                let occurence =
+                    Map.tryFind leftNum rightGroups
+                    |> Option.defaultValue 0
+                leftNum, (leftNum * occurence)
+            )
+            |> Array.sumBy snd
+        similarityScore
--- Tests.fs
+++ Tests.fs
@@ -3,8 +3,14 @@ module Tests
     open Xunit
 
     [<Fact>]
-
     let ``Day1 Part1`` () =
         let answer = Day1.Part1.solution "Day1-1.txt"
 
         Assert.Equal(2375403, answer)
+        // 23082277
+
+    [<Fact>]
+    let ``Day1 Part2`` () =
+        let answer = Day1.Part2.solution "Day1-1.txt"
+
+        Assert.Equal(23082277, answer)
