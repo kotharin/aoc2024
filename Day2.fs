b649404c43d8721dc4af90e771da15330fe03d0f
Neerav Kothari <neerav.kothari@gmail.com>
2024-12-12 16:15:25 -0500

    Day2 Part2, not working. Moving on for now.

--- Day2.fs
+++ Day2.fs
@@ -1,9 +1,6 @@
 namespace Day2
 
-module Part1 =
-    open System
-    open System.IO
-
+module Shared =
     type Direction =
         | Upward
         | Downward
@@ -12,6 +9,12 @@ module Part1 =
     type Status =
         | Safe
         | UnSafe
+
+
+module Part1 =
+    open System
+    open System.IO
+    open Shared
 
     let diffAndDirection x y =
         let diff = x - y
@@ -64,3 +67,90 @@ module Part1 =
         )
         |> Array.filter (fun status -> status = Safe)
         |> Array.length
+
+module Part2 =
+
+    open System
+    open System.IO
+    open Shared
+    type RowInfo = {
+        Numbers: int array
+        UpTransitions: List<(int*int)>
+        DownTransitions: List<(int*int)>
+        NoTransitions: List<(int*int)>
+        DiffViolations: List<(int*int)>
+    }
+
+    let transformRow numbers =
+
+        let rec transform row index  upt downt notsn diffV=
+            if (index = (Array.length row) ) then
+                // fill out the row info object
+                {
+                    Numbers = row;
+                    UpTransitions = upt;
+                    DownTransitions = downt;
+                    NoTransitions = notsn
+                    DiffViolations = diffV
+                }
+            else
+
+                let x,y = row.[index-1], row.[index]
+
+                // get the diff
+                let diff = abs(x - y)
+                let newDiffV =
+                    if ((diff < 1) ||(diff > 3)) then   
+                        List.append diffV [(index-1, index)]
+                    else diffV
+
+                // get the transitions
+                let newUpT =
+                    if (y > x) then
+                        List.append upt [(index-1, index)]
+                    else upt
+
+                let newDnT =
+                    if (x > y) then
+                        List.append downt [(index-1, index)]
+                    else downt
+
+                let newNoT =
+                    if (diff = 0) then
+                        List.append notsn [(index-1, index)]
+                    else notsn
+    
+                // move to next index
+                transform row (index+1) newUpT newDnT newNoT newDiffV
+
+        transform numbers 1 List.empty List.Empty List.empty List.Empty
+    
+    let hasAnyViolation (rowInfo:RowInfo) =
+        if ((rowInfo.DiffViolations.Length > 0) || (rowInfo.NoTransitions.Length > 0) || ((rowInfo.DownTransitions.Length > 0) && (rowInfo.UpTransitions.Length > 0) )) then
+            true
+        else false
+
+    (*
+    let has2orMoreViolations (rowInfo:RowInfo) =
+        if (rowInfo.NoTransitions.Length > 0) then
+    *)
+    let solution file =
+
+        let lines = File.ReadAllLines file
+
+        let rowsInfo =
+            lines
+            |> Array.mapi(fun i line ->
+                let nums =
+                    line.Split([|' '|])
+                    |> Array.map(int)
+                nums
+            )
+            |> Array.map(transformRow)
+
+        let safeRows =
+            rowsInfo
+            |> Array.filter(fun rowInfo -> not (hasAnyViolation rowInfo))
+
+        safeRows.Length
+        
--- Program.fs
+++ Program.fs
@@ -3,7 +3,7 @@ module Program =
     
     let [<EntryPoint>] main _ =
 
-        let answer = Day2.Part1.solution "Day2-1.txt"
+        let answer = Day2.Part2.solution "Day2-1.txt"
 
         printfn "answer: %i" answer
         0
